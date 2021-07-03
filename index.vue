f3b5c7a192fdfad346539b12a8a4aa70e71c48e9
igor-max <2141677845@qq.com>
2021-07-03 14:12:42 +0800

    merge

--- index.vue
+++ index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">DONT LOgin</h3>
+        <h3 class="title">lalalaalal</h3>
       </div>
 
       <el-form-item prop="username">
