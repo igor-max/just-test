ea0e747c42b6bae829b2349bcb1cb740614dc108
igor-max <51755357+igor-max@users.noreply.github.com>
2021-07-03 14:41:02 +0800

    Update test.vue

--- test.vue
+++ test.vue
@@ -107,26 +107,13 @@ export default {
       otherQuery: {}
     }
   },
-  watch: {
-    $route: {
-      handler: function(route) {
-        const query = route.query
-        if (query) {
-          this.redirect = query.redirect
-          this.otherQuery = this.getOtherQuery(query)
-        }
-      },
-      immediate: true
-    }
-  },
-  created() {
-    // window.addEventListener('storage', this.afterQRScan)
-  },
   mounted() {
     if (this.loginForm.username === '') {
       this.$refs.username.focus()
     } else if (this.loginForm.password === '') {
       this.$refs.password.focus()
+    } else {
+      console.log('sds');
     }
   },
   destroyed() {
